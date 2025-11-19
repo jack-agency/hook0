2e2b9b41c8eb1491fbcfc752d647277af88581b8
Brice Hemery <bmbiandja@gmail.com>
2025-11-19 13:13:53 +0100

    feat: update Docker image tags to use 'latest' for API and frontend services

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -41,7 +41,7 @@ variable "db_password" {
 variable "api_image" {
   description = "The image name to fetch from Artifact Registry"
   type        = string
-  default     = "hook0-api:v1.0.1"
+  default     = "hook0-api:latest"
 }
 
 variable "api_service_name" {
@@ -97,7 +97,7 @@ variable "master_api_key" {
 variable "frontend_image" {
   description = "The image name to fetch from Artifact Registry"
   type        = string
-  default     = "hook0-frontend:v1.0.1"
+  default     = "hook0-frontend:latest"
 }
 
 variable "frontend_service_name" {
@@ -117,7 +117,7 @@ variable "frontend_container_port" {
 variable "output_worker_image" {
   description = "The image name to fetch from Artifact Registry"
   type        = string
-  default     = "hook0-output-worker:v1.0.1"
+  default     = "hook0-output-worker:latest"
 }
 
 variable "output_worker_pool_name" {
